16edfb4d314dbf78e4c50f6a34de90a678030cdd
yasas <manukayasas99@gmail.com>
2021-04-30 19:05:22 +0530

    init

--- rest_services/card_service/routes/GatewayRoutes.js
+++
@@ -1,7 +0,0 @@
-const GatewayController = require("../controllers/GatewayController");
-
-const router = require("express").Router();
-
-router.post("/", GatewayController.makePayment);
-
-module.exports = router;
